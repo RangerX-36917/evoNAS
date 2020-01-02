90559f3e6eb6aedba46ccbdf6a6ad88e91f644c5
Znmangosteen <34234009+Znmangosteen@users.noreply.github.com>
2020-01-02 11:52:19 +0800

    [UPD] disable drawing part in NAS_main

--- program/NAS_main.py
+++ program/NAS_main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
-import seaborn as sns
+# import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+# import seaborn as sns
 import AgingEvolution
 import NonAgingEvolution
 import NewAgingEvolution
@@ -11,7 +11,7 @@ import random
 # import train
 
 # history = AgingEvolution.regularized_evolution(cycles=10000, population_size=10000, sample_size=50)
-sns.set_style('white')
+# sns.set_style('white')
 CYCLES = 500
 POPULATION_SIZE = 10
 SAMPLE_SIZE = 3
@@ -79,30 +79,30 @@ with open(dir + 'history', "wb") as f:
 # f.write(pickle.dumps(det_str))  
 # f.close()
 
-xvalues = range(len(history))
-yvalues = [i.accuracy for i in history]
-ax = plt.gca()
-ax.scatter(
-    xvalues, yvalues, marker='.', facecolor=(0.0, 0.0, 0.0),
-    edgecolor='b', linewidth=1, s=1)
+# xvalues = range(len(history))
+# yvalues = [i.accuracy for i in history]
+# ax = plt.gca()
+# ax.scatter(
+#     xvalues, yvalues, marker='.', facecolor=(0.0, 0.0, 0.0),
+#     edgecolor='b', linewidth=1, s=1)
+#
+# ax.xaxis.set_major_locator(ticker.LinearLocator(numticks=2))
+# ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
+# ax.yaxis.set_major_locator(ticker.LinearLocator(numticks=2))
+# ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+#
+# fig = plt.gcf()
+# fig.set_size_inches(8, 6)
+# fig.tight_layout()
+# ax.tick_params(
+#     axis='x', which='both', bottom='on', top='off', labelbottom='on',
+#     labeltop='off', labelsize=14, pad=10)
+# ax.tick_params(
+#     axis='y', which='both', left='on', right='off', labelleft='on',
+#     labelright='off', labelsize=14, pad=5)
+# plt.xlabel('Number of Models Evaluated', labelpad=-16, fontsize=16)
+# plt.ylabel('Accuracy', labelpad=-30, fontsize=16)
+# plt.xlim(0, 10000)
 
-ax.xaxis.set_major_locator(ticker.LinearLocator(numticks=2))
-ax.xaxis.set_major_formatter(ticker.ScalarFormatter())
-ax.yaxis.set_major_locator(ticker.LinearLocator(numticks=2))
-ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
-
-fig = plt.gcf()
-fig.set_size_inches(8, 6)
-fig.tight_layout()
-ax.tick_params(
-    axis='x', which='both', bottom='on', top='off', labelbottom='on',
-    labeltop='off', labelsize=14, pad=10)
-ax.tick_params(
-    axis='y', which='both', left='on', right='off', labelleft='on',
-    labelright='off', labelsize=14, pad=5)
-plt.xlabel('Number of Models Evaluated', labelpad=-16, fontsize=16)
-plt.ylabel('Accuracy', labelpad=-30, fontsize=16)
-plt.xlim(0, 10000)
-
-sns.despine()
+# sns.despine()
 # plt.show()
